6b94b11fd4133cdc98f67a19c3d2297107c6b068
Robert James Patterson <guajiropa@hotmail.com>
2018-10-10 22:07:39 -0400

    Fixing bugs found through tests not from text of the tutorial

--- bookstore/polls/admin.py
+++ bookstore/polls/admin.py
@@ -1,6 +1,6 @@
 """
 AUTHOR      :   Robert James Patterson
-DATE:       :   10/02/2018
+DATE        :   10/02/2018
 SYNOPSIS    :   Working thru the 'docs.djangoproject.com' tutorial
 """
 from django.contrib import admin
